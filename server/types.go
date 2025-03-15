b47c85e27e71ae88b4926f8972b451da1c3243e6
Max Leske <250711+theseion@users.noreply.github.com>
2025-03-15 15:24:40 +0100

    feat: add support for persistent endpoint configurations
    New endpoints:
      - /configure_reflections: configure reflection as for "/reflect" for
        any endpoint that is not predifened
      - /reset: reset all configured reflections
    
    Fixes #56

--- server/types.go
+++ server/types.go
@@ -3,3 +3,28 @@ package server
 type CapabilitiesSpec struct {
 	Endpoints []endpoint `json:"endpoints" yaml:"endpoints"`
 }
+
+type reflectionSpec struct {
+	Status      int               `json:"status"`
+	Headers     map[string]string `json:"headers"`
+	Body        string            `json:"body"`
+	EncodedBody string            `json:"encodedBody"`
+	LogMessage  string            `json:"logMessage"`
+}
+
+type configureReflectionSpec struct {
+	reflectionSpec
+	Endpoints []dynamicEndpointSpec `json:"endpoints"`
+}
+
+type dynamicEndpointSpec struct {
+	Method string `json:"method"`
+	Url    string `json:"url"`
+}
+
+type endpoint struct {
+	Path        string   `json:"path" yaml:"path"`
+	Methods     []string `json:"methods,omitempty" yaml:"methods"`
+	ContentType string   `json:"contentType,omitempty" yaml:"contentType"`
+	Description string   `json:"description,omitempty" yaml:"description"`
+}
